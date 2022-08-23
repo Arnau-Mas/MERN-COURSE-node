4d8dba77c8d83293de4c4327aa1e3483c4803263
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-23 12:21:17 +0200

    finished

--- citas-celebres-front-back/citas-celebres-react/src/App.jsx
+++ citas-celebres-front-back/citas-celebres-react/src/App.jsx
@@ -11,9 +11,11 @@ function App() {
 
   return (
     <div className="App">
-      <img style={{height:"200px", width:"200px", objectFit:"cover"}} src={`http://localhost:5000/img/${cita.image}`} alt="" />
+      <img style={{height:"400px", width:"400px", objectFit:"cover", position:"relative"}} src={`http://localhost:5000/img/${cita.image}`} alt="" />
+      <div style={{position:"absolute", bottom:"6rem", color:"white", width:"15rem", backgroundColor:"rgba(0,0,0,0.5)", display:"flex", flexDirection:"column", justifyContent:"center", paddingLeft:"0.8rem"}}>
       <p>{cita.quote}</p>
       <p>{cita.author}</p>
+      </div>
     </div>
   )
 }
