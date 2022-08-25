b3d01e82d8153d4f9bc107105a35af8c4e83154a
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-08-25 21:15:00 +0200

    deleting folder

--- ecommerce/vite-react-tailwind-starter-master/src/components/Filtres.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export const Filtres = () => {
-  return (
-    <div>Filtres</div>
-  )
-}
--- ecommerce/vite-react-tailwind-starter-master/src/components/LlistaArticles.jsx
+++
@@ -1,7 +0,0 @@
-export const LlistaArticles = () => {
-  return (
-    <div>
-        <Filtres/>
-    </div>
-  )
-}
